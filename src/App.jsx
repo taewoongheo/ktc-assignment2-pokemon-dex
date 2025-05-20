e8d3a90b51c751a9da1c26f1148a93fac8447adb
Taewoong Heo <chamjoeun0111@gmail.com>
2025-05-20 18:38:28 +0900

    feat: styled-components 로 스타일 reset 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
 import { Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Home from "./pages/Home.tsx";
 
-function Home() {
-  return <h1>Home Page</h1>;
-}
+export const ResetStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    font-family: sans-serif;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 function Dex() {
   return <h1>Dex Page</h1>;
 }
 
+function Layout({ children }) {
+  return <div style={{ width: "100%", height: "100%" }}>{children}</div>;
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dex" element={<Dex />} />
-    </Routes>
+    <>
+      <ResetStyle />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dex" element={<Dex />} />
+        </Routes>
+      </Layout>
+    </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <section>
+        <h1>Pokemon</h1>
+        <button>포켓몬 도감 시작하기</button>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
