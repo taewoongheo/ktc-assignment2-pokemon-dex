54f62f4844eff32eea0faa86c8bb3c6ee9bd7afc
Taewoong Heo <chamjoeun0111@gmail.com>
2025-05-21 12:45:14 +0900

    refactor(PokemonCard): PokemonCard 의 스타일을 분리하여 재사용하도록 변경

--- src/pages/Dex/components/Dashboard.jsx
+++ src/pages/Dex/components/Dashboard.jsx
@@ -1,59 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  AddButton,
+  PokemonId,
+  PokemonImage,
+  PokemonName,
+  StyledPokemonCard,
+} from "../styles/PokemonCard.styles";
 
 const Container = styled.div`
+  background-color: rgb(238, 238, 238);
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
+`;
+
+const DashboardGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  align-items: center;
-  justify-content: center;
   gap: 20px;
   margin-bottom: 20px;
-  background-color: rgb(238, 238, 238);
-  padding: 20px;
-  border-radius: 10px;
 `;
 
-const StyledPokemonCard = styled.div`
-  padding: 15px;
-  border-radius: 10px;
-  border: 1px solid rgb(213, 213, 213);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+const TitleContainer = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: rgb(255, 255, 255);
-`;
-
-const PokemonImage = styled.img`
-  width: 100px;
-  height: 100px;
-`;
-
-const PokemonName = styled.h1`
-  font-size: 16px;
-  font-weight: 600;
-  padding-top: 20px;
-  padding-bottom: 18px;
-`;
-
-const PokemonId = styled.p`
-  font-size: 16px;
-  font-weight: 600;
-  padding-bottom: 18px;
-`;
-
-const AddButton = styled.button`
-  padding: 5px 15px;
-  border-radius: 5px;
-  border: 1px solid rgb(213, 213, 213);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  font-size: 16px;
 `;
 
 function Dashboard({ selectedPokemon, setSelectedPokemon }) {
-  console.log(selectedPokemon);
-
   const deletePokemon = (id) => {
     setSelectedPokemon([
       ...selectedPokemon.filter((pokemon) => pokemon.id !== id),
@@ -62,18 +37,23 @@ function Dashboard({ selectedPokemon, setSelectedPokemon }) {
 
   return (
     <Container>
-      {selectedPokemon?.map((pokemon) => {
-        return (
-          <StyledPokemonCard>
-            <PokemonImage src={pokemon.image} alt={pokemon.name} />
-            <PokemonName>{pokemon.name}</PokemonName>
-            <PokemonId>No.{pokemon.id}</PokemonId>
-            <AddButton onClick={() => deletePokemon(pokemon.id)}>
-              삭제
-            </AddButton>
-          </StyledPokemonCard>
-        );
-      })}
+      <TitleContainer>
+        <h1>포켓몬 추가</h1>
+      </TitleContainer>
+      <DashboardGrid>
+        {selectedPokemon?.map((pokemon) => {
+          return (
+            <StyledPokemonCard>
+              <PokemonImage src={pokemon.image} alt={pokemon.name} />
+              <PokemonName>{pokemon.name}</PokemonName>
+              <PokemonId>No.{pokemon.id}</PokemonId>
+              <AddButton onClick={() => deletePokemon(pokemon.id)}>
+                삭제
+              </AddButton>
+            </StyledPokemonCard>
+          );
+        })}
+      </DashboardGrid>
     </Container>
   );
 }
