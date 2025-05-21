d2d86b0de41de0f01d833b7d829743c94f516fae
Taewoong Heo <chamjoeun0111@gmail.com>
2025-05-21 10:49:39 +0900

    style(Dex): 포켓몬 카드 스타일 추가

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -3,12 +3,9 @@ import styled from "styled-components";
 import { getPokemonInfoList } from "../api/api";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 100%;
   height: 100%;
+  padding: 5rem;
 `;
 
 const Dashboard = styled.div`
@@ -16,48 +13,63 @@ const Dashboard = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  flex: 0.3,
   background-color: red;
-  width: 100%;
 `;
 
 const PokemonList = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 20px;
   width: 100%;
-  background-color: yellow;
 `;
 
-const PokemonCard = styled.div`
+const StyledPokemonCard = styled.div`
+  padding: 15px;
+  border-radius: 10px;
+  border: 1px solid rgb(213, 213, 213);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
+`;
+
+const PokemonImage = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const PokemonName = styled.h1`
+  font-size: 16px;
+  font-weight: 600;
+  padding-top: 20px;
+  padding-bottom: 18px;
+`;
+
+const PokemonId = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  padding-bottom: 18px;
+`;
+
+const AddButton = styled.button`
+  padding: 5px 15px;
+  border-radius: 5px;
+  border: 1px solid rgb(213, 213, 213);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  font-size: 16px;
 `;
 
 function PokemonCardComponent({ pokemon }) {
-  const { id, name, description, image, types } = pokemon;
+  const { id, name, image } = pokemon;
 
   return (
-    <div style={{ backgroundColor: "blue" }}>
-      <img src={image} alt={name} />
-      <h1>{name}</h1>
-      <p>{id}</p>
-      <p>{description}</p>
-      <div>
-        {types === undefined ? (
-          <p>undefined</p>
-        ) : (
-          types.map((type) => {
-            return <div>{type.name}</div>;
-          })
-        )}
-      </div>
-    </div>
+    <StyledPokemonCard>
+      <PokemonImage src={image} alt={name} />
+      <PokemonName>{name}</PokemonName>
+      <PokemonId>No.{id}</PokemonId>
+      <AddButton>추가</AddButton>
+    </StyledPokemonCard>
   );
 }
 
@@ -79,7 +91,7 @@ function Dex() {
 
   return (
     <Container>
-      <Dashboard></Dashboard>
+      {/* <Dashboard></Dashboard> */}
       <PokemonList>
         {pokemonList.map((pokemon) => {
           return <PokemonCardComponent key={pokemon.id} pokemon={pokemon} />;
