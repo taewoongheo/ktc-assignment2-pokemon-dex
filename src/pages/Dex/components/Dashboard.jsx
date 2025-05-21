ac8fab19f83bdf5a551b1e6f02e5f90d011d7f95
Taewoong Heo <chamjoeun0111@gmail.com>
2025-05-21 13:11:49 +0900

    feat(Dashboard): 대시보드의 빈 항목에 포켓볼 svg 추가

--- src/pages/Dex/components/Dashboard.jsx
+++ src/pages/Dex/components/Dashboard.jsx
@@ -7,6 +7,7 @@ import {
   PokemonName,
   StyledPokemonCard,
 } from "../styles/PokemonCard.styles";
+import pokeball from "../../../assets/pokeball.svg";
 
 const Container = styled.div`
   background-color: rgb(238, 238, 238);
@@ -26,9 +27,33 @@ const TitleContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 20px;
 `;
 
+const PokeballContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PokeballImage = styled.img`
+  width: 80px;
+  height: 80px;
+`;
+
+function Pokeball() {
+  return (
+    <PokeballContainer>
+      <PokeballImage src={pokeball} alt="Pokeball" />
+    </PokeballContainer>
+  );
+}
 function Dashboard({ selectedPokemon, setSelectedPokemon }) {
+  const list = Array.from({ length: 6 }, () => null);
+  for (let i = 0; i < selectedPokemon.length; i++) {
+    list[i] = selectedPokemon[i];
+  }
+
   const deletePokemon = (id) => {
     setSelectedPokemon([
       ...selectedPokemon.filter((pokemon) => pokemon.id !== id),
@@ -38,20 +63,23 @@ function Dashboard({ selectedPokemon, setSelectedPokemon }) {
   return (
     <Container>
       <TitleContainer>
-        <h1>포켓몬 추가</h1>
+        <h1>나만의 포켓몬</h1>
       </TitleContainer>
       <DashboardGrid>
-        {selectedPokemon?.map((pokemon) => {
-          return (
-            <StyledPokemonCard>
-              <PokemonImage src={pokemon.image} alt={pokemon.name} />
-              <PokemonName>{pokemon.name}</PokemonName>
-              <PokemonId>No.{pokemon.id}</PokemonId>
-              <AddButton onClick={() => deletePokemon(pokemon.id)}>
-                삭제
-              </AddButton>
-            </StyledPokemonCard>
-          );
+        {list.map((pokemon) => {
+          if (pokemon) {
+            return (
+              <StyledPokemonCard>
+                <PokemonImage src={pokemon.image} alt={pokemon.name} />
+                <PokemonName>{pokemon.name}</PokemonName>
+                <PokemonId>No.{pokemon.id}</PokemonId>
+                <AddButton onClick={() => deletePokemon(pokemon.id)}>
+                  삭제
+                </AddButton>
+              </StyledPokemonCard>
+            );
+          }
+          return <Pokeball />;
         })}
       </DashboardGrid>
     </Container>
