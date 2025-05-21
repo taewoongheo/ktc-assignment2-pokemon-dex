0bd5b79a1478afaf9ea298cb8ba19ca7228ec8b6
Taewoong Heo <chamjoeun0111@gmail.com>
2025-05-21 11:42:58 +0900

    feat(Dashboard): 포켓몬을 6개 초과하여 선택 시 알람 기능 추가

--- src/pages/Dex/components/PokemonCard.jsx
+++ src/pages/Dex/components/PokemonCard.jsx
@@ -41,16 +41,27 @@ const AddButton = styled.button`
 function PokemonCard({ pokemon, setSelectedPokemon }) {
   const { id, name, image } = pokemon;
 
+  const addPokemon = (id) => {
+    setSelectedPokemon((prev) => {
+      if (prev.length === 6) {
+        alert("포켓몬은 최대 6마리까지만 추가할 수 있습니다.");
+        return prev;
+      }
+
+      if (prev.find((pokemon) => pokemon.id === id)) {
+        return prev;
+      } else {
+        return [...prev, pokemon];
+      }
+    });
+  };
+
   return (
     <StyledPokemonCard>
       <PokemonImage src={image} alt={name} />
       <PokemonName>{name}</PokemonName>
       <PokemonId>No.{id}</PokemonId>
-      <AddButton
-        onClick={() => setSelectedPokemon((prev) => [...prev, pokemon])}
-      >
-        추가
-      </AddButton>
+      <AddButton onClick={() => addPokemon(id)}>추가</AddButton>
     </StyledPokemonCard>
   );
 }
